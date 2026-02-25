4884c2865c610da735edc8604d8fa8d8dffacb1a
Usuario <usuario@example.com>
2026-02-25 17:31:14 -0300

    Deploy: Atualiza API_URL para o Render

--- public/app.js
+++ public/app.js
@@ -1,4 +1,4 @@
-const API_URL = 'http://localhost:3000';
+const API_URL = 'https://platinacoolside.onrender.com';
 
 const playerSelect = document.getElementById('playerSelect');
 const gameNameInput = document.getElementById('gameName');
